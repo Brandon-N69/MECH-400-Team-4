451f35c5ebec1073cdee09c8e8df7740e5f570de
D-A-Wallace <135087614+D-A-Wallace@users.noreply.github.com>
2023-07-12 01:00:40 -0700

    updated calculation files

--- Calculations/MATLAB/cals.m
+++ Calculations/MATLAB/cals.m
@@ -18,9 +18,11 @@ g = 9.806;
 
 % Ratio by volume of air to water
 R_air_water = 0.71;
-% Ratio of total areated volume as air
-R_air = R_air_water/(1 + R_air_water);
-R_water  = 
+% Ratio of total areated volume
+	% as air
+	R_air = R_air_water/(1 + R_air_water);
+	% as water
+	R_water  = 1 - R_air;
 
 % manifold piping
 	% flanged
@@ -38,6 +40,32 @@ R_water  =
 
 %% loadcase
 
-F_y_g_manifold = - g*m_manifold;
+%12 inch T
+pipe_12_T.m = 1.950702*(10^(1));		%Mass (kg)
+pipe_12_T.V = 2.049057134*(10^(-2));	%Pipe Volume (m3)
+pipe_12_T.Vi = 0;						%internal volume
+pipe_12_T.c = [0, 0.11424778, 0];		%centroid distance from origin (m)
+pipe_12_T.d = [0.365125, 0.527050, 0];	%distance from origin to next
 
-	
+%injector assembly
+pipe_injector.m = 1.823*(10^(1));
+pipe_injector.V = 1.574742806*(10^(-2));
+pipe_injector.Vi = 0;
+pipe_injector.c = [0,0,0.233];
+pipe_injector.d = [0.2921,0,0];
+
+%flanged connection
+connect_fx.m = 3.251*(10^(1));
+connect_fx.V = 1.512*(10^(-2));
+connect_fx.Vi = 0;
+connect_fx.c = [0,0,0];
+connect_fx.d = [0.27432,0,0];
+
+% horizontal pipe
+pipe_x.m = 1.006*(10^(1));
+pipe_x.V = 1.057*(10^(-2));
+pipe_x.Vi = 0;
+pipe_x.c = connect_fx.c;
+pipe_x.d = connect_fx.d;
+
+
